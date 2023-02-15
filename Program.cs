02ed23bd2ed1826f6ee6359971f96c584c6c1b19
Zo PHPCoder99 <gergenkopzoya@gmail.com>
2023-02-15 20:46:20 +0700

    solved 3 tasks

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace gbhw2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1...
+            // Console.Write("Enter number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // if (num/1000 == 0 && num/100 != 0){
+            //     Console.WriteLine(num/100);
+            //     Console.WriteLine((num%100-num%10)/10+"");
+            // } else {
+            //     Console.WriteLine("The number does not have three digits.");
+            // }
+
+            // 2...
+            // Console.Write("Enter number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // if (num/100 != 0){
+            //     Console.WriteLine((num%1000-num%100)/100+"");
+            // } else {
+            //     Console.WriteLine("The number does not have a third digit.");
+            // }
+
+            // 3...
+            // Console.Write("Enter number: ");
+            // int num = Convert.ToInt32(Console.ReadLine());
+            // if (num >= 1 && num <= 5){
+            //     Console.WriteLine("no");
+            // } else if (num == 6 || num == 7){
+            //     Console.WriteLine("yes");
+            // } else {
+            //     Console.WriteLine("day is not valid");
+            // }
+        }
+    }
+}
